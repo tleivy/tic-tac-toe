3fbe681c3acb72ad4dbe37ec2567151c994d6778
tleivy <tomerleivy1@gmail.com>
2024-12-27 16:55:27 +0200

    Added more used cell error strings

--- Strings.java
+++ Strings.java
@@ -2,4 +2,6 @@ public class Strings {
     public static final String rowOutOfBounds = "Your selected row is out of bounds. Please try again.";
     public static final String colOutOfBounds = "Your selected column is out of bounds. Please try again.";
     public static final String illegalSign = "Signs must be only X/O.";
+    public static final String userCellAlreadyVisted = "Oops! You have already claimed this cell.";
+    public static final String opponentCellAlreadyVisted = "Oops! Your oponent have already claimed this cell.";
 }
