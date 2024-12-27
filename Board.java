571b02f9d319dcf654f9a19b5a53e5c403a7169e
tleivy <tomerleivy1@gmail.com>
2024-12-27 17:02:26 +0200

    Added empty matrix initialization for Board

--- Board.java
+++ Board.java
@@ -1,10 +1,23 @@
+import java.util.HashMap;
+
 public class Board {
     private final int size = 3;
     private final int asciiOfA = 65;
+    private final HashMap<Character, Character> opponentSign = new HashMap<>() {
+        {
+            put('X', 'O');
+            put('O', 'X');
+        }
+    };
     private char[][] matrix;
 
     public Board() {
         this.matrix = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                this.matrix[i][j] = ' ';
+            }
+        }
     }
 
     public char[][] getMatrix() {
@@ -12,6 +25,17 @@ public class Board {
     }
 
     public void processUserMove(char row, int col, char sign)
+            throws GameExceptions.IllegalMoveException, GameExceptions.IllegalSignException,
+            GameExceptions.CellAlreadyUsed, GameExceptions.GarbageValueInCell {
+        int rowValue = (int) row - asciiOfA;
+        int colValue = col - 1;
+        if (validateUserInputInBounds(row, col, sign) && validateCellIsEmpty(rowValue, colValue, sign)) {
+            this.matrix[rowValue][colValue] = sign;
+        }
+
+    }
+
+    private boolean validateUserInputInBounds(char row, int col, char sign)
             throws GameExceptions.IllegalMoveException, GameExceptions.IllegalSignException {
         if (row < 'A' || row > 'C') {
             throw new GameExceptions.IllegalMoveException(Strings.rowOutOfBounds);
@@ -20,8 +44,18 @@ public class Board {
         } else if (sign != 'X' && sign != 'O') {
             throw new GameExceptions.IllegalSignException(Strings.colOutOfBounds);
         }
-        int rowValue = (int) row;
-        this.matrix[rowValue - asciiOfA][col - 1] = sign;
+        return true;
     }
 
+    private boolean validateCellIsEmpty(int row, int col, char sign)
+            throws GameExceptions.CellAlreadyUsed, GameExceptions.GarbageValueInCell {
+        if (this.matrix[row][col] == sign) {
+            throw new GameExceptions.CellAlreadyUsed(Strings.userCellAlreadyVisted);
+        } else if (this.matrix[row][col] == opponentSign.get(sign)) {
+            throw new GameExceptions.CellAlreadyUsed(Strings.opponentCellAlreadyVisted);
+        } else if (this.matrix[row][col] != ' ') {
+            throw new GameExceptions.GarbageValueInCell(Strings.garbageValueInCell);
+        }
+        return true;
+    }
 }
