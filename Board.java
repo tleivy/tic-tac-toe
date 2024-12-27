8926564a49850848e300b42f78b0fb60ad53c619
tleivy <tomerleivy1@gmail.com>
2024-12-27 17:58:48 +0200

    Added check winner and reset methods

--- Board.java
+++ Board.java
@@ -10,18 +10,18 @@ public class Board {
         }
     };
     private char[][] matrix;
+    private char winner;
 
     public Board() {
-        this.matrix = new char[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                this.matrix[i][j] = ' ';
-            }
-        }
+        nullifyGame();
     }
 
     public char[][] getMatrix() {
         return this.matrix;
+    }
+
+    public char getWinner() {
+        return this.winner;
     }
 
     public void processUserMove(char row, int col, char sign)
@@ -32,7 +32,29 @@ public class Board {
         if (validateUserInputInBounds(row, col, sign) && validateCellIsEmpty(rowValue, colValue, sign)) {
             this.matrix[rowValue][colValue] = sign;
         }
+    }
 
+    /**
+     * This method checks if there's a triple of same sign in the board.
+     * As a side effect it updates the winner, if there is one.
+     * @return true if there's a winner, false otherwise.
+     */
+    public boolean checkIfSomeoneWon() {
+        return isRowWinner() || isColWinner() || isDiagonalWinner();
+    }
+
+    public void resetBoard() {
+        nullifyGame();
+    }
+
+    private void nullifyGame() {
+        this.matrix = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                this.matrix[i][j] = ' ';
+            }
+        }
+        this.winner = ' ';
     }
 
     private boolean validateUserInputInBounds(char row, int col, char sign)
@@ -57,5 +79,37 @@ public class Board {
             throw new GameExceptions.GarbageValueInCell(Strings.garbageValueInCell);
         }
         return true;
+    }
+
+    private boolean isRowWinner() {
+        for (int i = 0; i < this.size; i++) {
+            if (this.matrix[i][0] == this.matrix[i][1] && this.matrix[i][0] == this.matrix[i][2]) {
+                this.winner = this.matrix[i][0];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isColWinner() {
+        for (int j = 0; j < this.size; j++) {
+            if (this.matrix[0][j] == this.matrix[1][j] && this.matrix[0][j] == this.matrix[2][j]) {
+                this.winner = this.matrix[0][j];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isDiagonalWinner() {
+        if (this.matrix[0][0] == this.matrix[1][1] && this.matrix[0][0] == this.matrix[2][2]) {
+            this.winner = this.matrix[0][0];
+            return true;
+        }
+        if (this.matrix[2][0] == this.matrix[1][1] && this.matrix[2][0] == this.matrix[2][0]) {
+            this.winner = this.matrix[0][0];
+            return true;
+        }
+        return false;
     }
 }
